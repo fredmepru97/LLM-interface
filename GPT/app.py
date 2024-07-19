1a2b9298c2b7684ef0022dcdf9956f422d1adb82
MRaza98 <raza.1998@hotmail.com>
2024-07-19 11:24:56 +0200

    CHANGES

--- GPT/app.py
+++ GPT/app.py
@@ -11,18 +11,72 @@ load_dotenv(dotenv_path=env_path)
 # Get the OpenAI API key from environment variables
 api_key = os.getenv("OPENAI_API_KEY")
 
-# Column explanations
-column_explanations = {
-    "journal_akronym": "The acronym of the journal in which the paper was published.",
-    "article_id": "A unique identifier for each article.",
-    "citekey": "A citation key used to reference the article.",
-    "abstract": "A brief summary of the article.",
-    "journal_akronym": "The acronym of the journal where the article was published.",
-    "citation_count": "The number of times the article has been cited by other papers.",
-    "sentence_id": "A unique identifier for each sentence in the article.",
-    "section_id": "A unique identifier for each section in the article.",
-    "sentence_original": "The original text of the sentence.",
-    # Add more explanations as needed
+additional_info = {
+    "papers": {
+        "purpose": "Master Data of all papers in our database. One row is one paper.",
+        "columns": {
+            "article_id": "unique identifier for an article; used in many other tables as well; can be used for joining data with data from other tables",
+            "citekey": """Some tables only have a citekey and no article_id, then you can use the citeley to join the tables 
+                (stays the same though different runs)""",
+            "abstract": "text paragraph with the complete abstract of a research article",
+            "journal_akronym": "abbreviation of the journal name",
+            "citation_count": "how often was the paper cited from papers in our database",
+        }
+    },
+    "sentences": {
+        "purpose": "All individual sentences of every paper in the database. one row is one sentence.",
+        "columns": {
+            "article_id": "can be used for joining with other tables",
+            "sentence_id": "unique sentence_id for each individual sentence",
+            "section_id": "Can be used to join with section table to get the name of the section name",
+            "last_section_title": "section name",
+            "last_subsection_title": "subsection name",
+            "section_nr": "section counter for each article (starts with 0 (or 1) for each article)",
+            "para_id": "can be used to join with paragraph table",
+            "sentence_type": "refers to different sections in the paper (abstract, etc.) but is not as detailed as last_section_title",
+            "sentence_original": "The text of the sentence",
+        }
+    },
+    "paragraphs": {
+        "purpose": "All individual paragraphs of every paper in the database. one row is one paragraph. one paragraph can have multiple sentences.",
+        "columns": {
+            "para_id": "unique para_id for each individual paragraph"
+        }
+    },
+    "entities": {
+        "purpose": """All individual entities from the ontology that we found in any paper of the database entity (see column 'entity', not 'ent_id'). 
+                    One row is one appearance of an entity in an article. 
+                    If 'survey' appears multiple times in a paper, we get multiple rows.""",
+        "columns": {
+            "ent_id": "the ent_id from an entity in the IS Ontology, see",
+            "entity": """The synonym that was used in a text (not the direct name of an ent_id, but a synonym for an ent_id - see table synonyms),
+                 e.g. entity='platform strategy', ent_id='digital platform'""",
+            "sentence": "modified sentence where we replaced acronyms with the full name"
+        }
+    },
+    "ontology": {
+        "purpose": "All individual ent_ids from the IS Ontology. one row is for one ent_id",
+        "columns": {
+            "ent_id": "that is a unique entity (key term) in the IS Ontology, e.g., survey, case study, etc.",
+            "definition": "definition for an ent_id (generated with GPT 4)",
+            "label": "top level descriptions of an ontology branch"
+        }
+    },
+    "citations": {
+        "purpose": """All individual citations from our database. If an author of an article cites another paper somewhere in the text, we create an entry in this table.
+                    one row is one individual citation""",
+        "columns": {
+            "reference_citekey": "what article is cited? This is the citekey in table sources",
+            "paper_citekey": "links to table papers"
+        }
+    },
+    "sources": {
+        "purpose": """All individual sources that come from the reference section of papers in our database. one row is one source""",
+        "columns": {
+            "citekey": "the reference_citekey from table citations",
+            "para_id": "unique para_id for each individual paragraph"
+        }
+    }
 }
 
 if not api_key:
@@ -34,14 +88,25 @@ else:
     client = openai.OpenAI(api_key=api_key)
 
     def fetch_schema_info():
-        tables = conn.execute("SHOW TABLES").fetchall()
-        schema_info = {}
-        for table in tables:
-            table_name = table[0]
-            columns = conn.execute(f"DESCRIBE main.{table_name}").fetchall()
-            schema_info[table_name] = [column[0] for column in columns]
-        return schema_info
-    
+        try:
+            tables = conn.execute("SHOW TABLES").fetchall()
+            schema_info = {}
+            for table in tables:
+                table_name = table[0]
+                columns = conn.execute(f"DESCRIBE {table_name}").fetchall()
+                schema_info[table_name] = {
+                    "columns": {column[0]: "" for column in columns}
+                }
+                # Add additional info if available
+                if table_name in additional_info:
+                    schema_info[table_name]["purpose"] = additional_info[table_name].get("purpose", "No purpose available")
+                    schema_info[table_name]["columns"].update(additional_info[table_name].get("columns", {}))
+            return schema_info
+        except Exception as e:
+            st.error(f"Error fetching schema information: {e}")
+            return None
+
+    # Fetch schema information
     schema_info = fetch_schema_info()
 
     st.title("LLM Interface for Databases")
@@ -74,10 +139,15 @@ else:
     query = st.text_area('Enter your text to generate SQL query', '')
 
     def generate_sql(prompt, schema_info):
-        schema_info_str = "\n".join([f"Table '{table}': columns {', '.join(columns)}" for table, columns in schema_info.items()])
+        schema_info_str = "\n".join(
+            [f"Table '{table}': Purpose: {info.get('purpose', 'N/A')}\nColumns: {', '.join([f'{col}: {desc}' for col, desc in info['columns'].items()])}" 
+             for table, info in schema_info.items()])
         enhanced_prompt = f"""
-                {schema_info_str}\n\nGenerate a SQL query to {prompt}, alias the columns in the SELECT statement extremely precicely.
-                Do not include any non SQL related characters."""
+                {schema_info_str}\n\nGenerate a SQL query (DuckDB dialect) to {prompt}, alias the columns in the SELECT statement extremely precicely.
+                Do not include any non SQL related characters. While generating the SQL query, consider any edge cases the prompt may have.
+                E.g. if a prompt is asking for a column name, consider the possibility that the column name may have a space in it. Or, if a prompt
+                is asking about how many articles mention the phrase business intelligence, then you must also consider where B of business and I
+                of intelligence are capitalized."""
 
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
@@ -113,17 +183,18 @@ else:
     def prompt_to_sql_execution(base_prompt, query, schema_info):
         full_prompt = f"{base_prompt} {query}"
         sql_query = generate_sql(full_prompt, schema_info)
-        print(f"Generated SQL: {sql_query}")
         result = execute_sql(sql_query)
         return sql_query, result
+    
+    base_prompt = "Generate a SQL query to of the following input. Only generate SQL query."
 
     def summarize_results(results, base_prompt, query):
         summary = " \n\n"
         full_prompt = f"{base_prompt} {query}"
         # Summarize the content
         content_summary_prompt = f"""You have the results:\n\n{results.to_string(index=False)} to the {full_prompt} query which were
-                                generated via a DuckDB database, please tell me how these results compare to what you know about this certain topic
-                                without this additional information."""
+                                generated via RAG. Please tell me how these results compare to what you know about this prompt
+                                without this additional information from the RAG."""
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -137,9 +208,11 @@ else:
         
         summary += f"\n\n{content_summary}"
         return summary
-    base_prompt = "Generate a SQL query to of the following input. Only generate SQL query."
+    
     if st.button('Generate SQL query'):
         if len(query) > 0:
+            full_prompt = f"{base_prompt} {query}"
+            print(full_prompt)
             sql_query, result = prompt_to_sql_execution(base_prompt, query, schema_info)
             st.write("Generated SQL Query:")
             st.code(sql_query, language='sql')

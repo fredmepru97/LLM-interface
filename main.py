addc324ab9affe6f1ebef492bc8c9cf12a4647be
MRaza98 <raza.1998@hotmail.com>
2024-07-21 13:17:57 +0200

    adjusted home page descriptions

--- main.py
+++ main.py
@@ -9,20 +9,16 @@ def main():
     page = st.sidebar.radio("Go to", ["Home Page", "GPT", "Llama"])
 
     if page == "Home Page":
-        st.title("Welcome to our home page!")
+        st.title("Welcome to LLM Interface for Databases!") 
         st.text("-------------------------------------------------------------------------------")
         st.text("-------------- A Text, Web and Social Media Analytics Lab Project -------------")
         st.text("-------------- Team members: Jessica, Sarah, Raza, Victor, Freddy -------------")
         st.text("-------------------------------------------------------------------------------")
         st.write(" ")
-        st.write("Our project uses GPT and LLama to convert natural language to SQL queries to later query a Duckdb database.")
-        st.write("- You can navigate to the GPT page to use GPT-3.5 Turbo to generate SQL queries.")
-        st.write("- You can navigate to the LLama page to use LLama to generate SQL queries.")
-        st.write("- You can navigate to the Simple GPT page to use GPT-3.5 Turbo to generate SQL queries without a connection to our database.")
-        st.write("- You can navigate to the Simple LLama page to use LLama to generate SQL queries without a connection to our database.")
-        st.text(" ")
+        st.write("Our project uses GPT and LLama to convert natural language to SQL queries to later query a DuckDB database.")
+        st.write("- You can navigate to the GPT page to use GPT-3.5 Turbo to generate SQL queries using both zero-shot and one-shot prompting strategies.")
+        st.write("- You can navigate to the GPT page to use Llama 3 70B to generate SQL queries using both zero-shot and one-shot prompting strategies.")
         st.text("-------------------------------------------------------------------------------")
-        st.text("---------------------------------- July 2024 ----------------------------------")
         
     elif page == "GPT":
             gpt_zero_shot_app()
